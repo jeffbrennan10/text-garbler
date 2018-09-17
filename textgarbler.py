d74e3706b33e9fef48467b9271a5cb12272766a4
Jeff Brennan <jeffbrennan10@gmail.com>
2018-09-17 11:12:39 -0500

    removed looping function, add language framework, actually can work now

--- textgarbler.py
+++ textgarbler.py
@@ -10,90 +10,57 @@ def text_compare (start_text, end_text):
     ratio = difflib.SequenceMatcher(None, start_text, end_text).ratio()
     return ratio
 
-def spanishconverter(text):
-    return (text.replace('%C3%A1','á').replace("%C3%A9",'é').replace('%C3%AD','í').replace('%C3%B3','ó')
+def languageConverter(text, language):
+    if language == 'en':
+        return text
+    elif language == 'es': ##spanish
+        return (text.replace('%C3%A1','á').replace("%C3%A9",'é').replace('%C3%AD','í').replace('%C3%B3','ó')
             .replace('%C3%BA','ú').replace('%C3%B1','ñ').replace('%C3%BC','ü').replace('%C3%81','Á')
             .replace('%C3%89','É').replace('%C3%8D','Í').replace('%C3%93','Ó').replace('%C3%9A','Ú')
             .replace('%C2%A1','¡').replace('%C2%BF','¿'))
+    # elif language == 'sv': ## swedish
+    # elif language == 'sl': ## slovenian
 
 def text_encoder (text, lang):
     text = urllib.parse.quote(text)
     cleantext = text.replace('.', '')
-    if lang == 'es':
-        return spanishconverter(cleantext)
+    return languageConverter(cleantext, lang)
     
-    return cleantext
 
 def text_decoder (text):
     cleantext = text.replace('.', '')
     return urllib.parse.unquote(cleantext)
 
 
-def translate_request (text): 
+def translate_request (inputText, inLang, outLang): 
     driver = webdriver.Chrome(chrome_path, chrome_options=options)
-    driver.get('https://translate.google.com/#en/' + f_lang + '/' + start_text)
-    try:
-        return driver.find_element_by_xpath('//*[@id="result_box"]/span').text
-    except:
-        return driver.find_element_by_xpath('//*[@id="result_box"]/span/text()').text
+    driver.get('https://translate.google.com/#' + inLang + '/' + outLang + '/' + inputText) ## converts betweeen input language and output language
 
-def translation_garble(start_text, f_lang):
+    translationResult = driver.find_element_by_id("result_box")
+    driver.quit
+    return translationResult.get_attribute('textContent')
 
+# english -> spanish -> swedish -> slovenian -> english
+def translation_garble(start_text):
     counter = 0
     start_text = text_encoder(start_text,'eng')
-
     print (start_text)
 
-    driver = webdriver.Chrome(chrome_path, chrome_options=options)
-    driver.get('https://translate.google.com/#en/' + f_lang + '/' + start_text)
-    foreignText = driver.find_element_by_xpath('//*[@id="result_box"]/span').text
-    driver.quit()
-    print (foreignText)
+    foreignText = (translate_request(start_text,'eng', 'es'))
+    # print (foreignText)
+    # foreignText = text_encoder(foreignText, 'es')
 
-    foreignText = text_encoder(foreignText,'es')
 
-    print (foreignText)
-
-    # driver = webdriver.Chrome(chrome_path, chrome_options=options)
-    # driver.get('https://translate.google.com/#'+ f_lang + '/en/' + foreignText)
-    # nativeText = driver.find_element_by_xpath('//*[@id="result_box"]/span').text
-    # driver.quit()
-    # nativeText = text_encoder(nativeText,'eng')
-
+    # nativeText = (translate_request(start_text, 'eng'))
     # print (nativeText)
+    # nativeText = text_encoder(foreignText, 'eng')
 
-    # for _ in range(loopnumber):
-    #     driver = webdriver.Chrome(chrome_path, chrome_options=options)
-    #     driver.get('https://translate.google.com/#en/' + f_lang + '/' + nativeText)
-    #     foreignText = driver.find_element_by_xpath('//*[@id="result_box"]/span').text
-        
-    #     driver.quit()
-
-    #     foreignText = text_encoder(foreignText,'es')
-
-    #     print ('loop ftext' + foreignText)
-
-    #     driver = webdriver.Chrome(chrome_path, chrome_options=options)
-    #     driver.get('https://translate.google.com/#'+ f_lang + '/en/' + foreignText)
-    #     nativeText = driver.find_element_by_xpath('//*[@id="result_box"]/span').text
-
-    #     nativeText = text_encoder(nativeText,'eng')
-
-    #     counter +=1
-    #     print (counter)
-    
-    # print ('Text similarity:' + "{0:.2f}".format(text_compare(start_text, nativeText)))
     # print ('Your text was garbled ' + str(counter) + ' times:')
     # print(text_decoder(nativeText))
 
 print ('Enter desired text:')
 start_text = input()
 
-print ('Enter desired number of translation exchanges')
-loopnumber = int(input())
-
-print ('Enter desired foriegn language. Spanish: es')
-f_lang = input()
-
 counter = 0
-translation_garble(start_text, f_lang)
+languageList = ['eng', 'es', 'sv', 'sl', 'eng']
+translation_garble(start_text)
